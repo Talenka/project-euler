e17326bbf2928d6b773f59dc29fc443e521b377d
Boudah Talenka <boudah.talenka@gmail.com>
2014-01-16 16:24:07 +0100

    Problem 6 solved

---
+++ problem6.js
@@ -0,0 +1,37 @@
+/**
+ * Return the difference between the square of the sum of integers n < 100,
+ * A(n), and the sum of squared integers, B(n).
+ *
+ * Source: https://projecteuler.net/problem=6
+ *
+ * A(n) = 1 + 4 + 9 + ... + n^2
+ * B(n) = (1 + 2 + 3 + ... + n)^2
+ *
+ * We are looking for D(n) = B(n) - A(n), with n = 100.
+ *
+ * The strategy is to study how A, B, and D grow versus n.
+ *
+ * A(n + 1) = A(n) + (n + 1)^2
+ *
+ * Let X = 1 + 2 + 3 + ... + n = n * (n + 1) / 2
+ *
+ * B(n + 1) = (X + (n + 1))^2
+ *          = B(n) + (n + 1)^2 + 2 * X * (n + 1)
+ *          = B(n) + (n + 1)^3
+ *
+ * Hence:
+ * D(n + 1) - D(n) = (n + 1)^3 - (n + 1)^2 = n * (n + 1)^2
+ *
+ * Finally, all we have to do is to sum this formula over integers n < 100.
+ *
+ * @return {number}
+ */
+function problem6()
+{
+  var difference = 0,
+      n;
+
+  for (n = 1; n <= 100; n++) difference += (n - 1) * n * n;
+
+  return difference; // --> 25164150
+}
