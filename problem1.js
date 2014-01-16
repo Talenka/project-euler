e56b48afb4c6b4107bca4e1b92f50e6d13927c14
Boudah Talenka <boudah.talenka@gmail.com>
2014-01-16 16:23:27 +0100

    Problem 1 solved

---
+++ problem1.js
@@ -0,0 +1,16 @@
+/**
+ * Return the sum of integers n < 1000, which are multiples of 3 or 5.
+ *
+ * Source: https://projecteuler.net/problem=1
+ * @return {number}
+ */
+function problem1()
+{
+  var n, sum = 0;
+
+  for (n = 3; n < 1000; n++) {
+    if (n % 3 == 0 || n % 5 == 0) sum += n;
+  }
+
+  return sum; // --> 233168
+}
