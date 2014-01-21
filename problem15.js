943939d9c7217418c7cce4c8fba868c0f2f151a4
Boudah Talenka <boudah.talenka@gmail.com>
2014-01-21 12:37:20 +0100

    Problem 15 solved

---
+++ problem15.js
@@ -0,0 +1,23 @@
+/**
+ * Starting in the top left corner of a 2 * 2 grid, and only being able to move
+ * to the right and down, there are exactly 6 routes to the bottom right corner.
+ *
+ * How many such routes are there through a N * N grid, with N = 20?
+ *
+ * This is just the number of permutations with repetition.
+ * (See https://fr.wikipedia.org/wiki/Permutation_avec_r%C3%A9p%C3%A9tition)
+ *
+ * Here this is just (2N)! / (N!)^2
+ *
+ * Source : https://projecteuler.net/problem=15
+ *
+ * Result: 137846528820
+ * Time: 18ms
+ */
+
+var routesNumber = 1, N = 20;
+
+for (i = 1; i <= N; i++) routesNumber *= (N + i) / i;
+
+self.postMessage({type: 'result',
+                  result: 'Routes number: ' + routesNumber});
