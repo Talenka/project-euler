59de59bc2a10b199569fb53842a1ebcac51f2ac7
Boudah Talenka <boudah.talenka@gmail.com>
2014-01-21 13:14:50 +0100

    Problem 16 solved

---
+++ problem16.js
@@ -0,0 +1,41 @@
+/**
+ * What is the sum of the digits of the number 2^1000?
+ *
+ * Source : https://projecteuler.net/problem=16
+ *
+ * Result: 1366
+ * Time: 18ms
+ */
+
+var digits = [1],
+    i,
+    digitsSum = 0,
+    ind = false;
+
+for (i = 1; i <= 1000; i++) {
+
+  self.postMessage({type: 'progress', current: i, max: 15});
+
+  for (d = 0, dl = digits.length; d < dl; d++) {
+
+    digits[d] *= 2;
+
+    if (ind) digits[d]++;
+
+    if (digits[d] >= 10) {
+
+      ind = true;
+      digits[d] -= 10;
+
+      if (d == dl - 1) {
+        digits.push(1);
+        ind = false;
+      }
+
+    } else ind = false;
+  }
+}
+
+for (d = 0, dl = digits.length; d < dl; d++) digitsSum += digits[d];
+
+self.postMessage({type: 'result', result: 'Digits sum: ' + digitsSum});
