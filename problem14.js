050163698c428ffbc35def023c95c9766e18f9b6
Boudah Talenka <boudah.talenka@gmail.com>
2014-01-21 11:39:12 +0100

    Problem 14 solved

---
+++ problem14.js
@@ -0,0 +1,52 @@
+/**
+ * The following iterative sequence is defined for the set of positive integers:
+ *
+ * n --> n / 2        (if n is even)
+ * n --> 3 * n + 1    (if n is odd)
+ *
+ * Which starting number i < 1000000, produces the longest chain?
+ *
+ * Strategy: brute force
+ *
+ * Source : https://projecteuler.net/problem=14
+ *
+ * Result: 837799 (525-long chain)
+ * Time: 2.46s
+ */
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+function chainLength(n)
+{
+  chain = 1;
+
+  while (n > 1) {
+    if (n % 2 == 0) n /= 2;
+    else n = 3 * n + 1;
+    chain++;
+  }
+
+  return chain;
+}
+
+
+var i, longestChain = 1, longestChainStart, chain;
+
+for (i = 1; i < 1000000; i++) {
+
+  if (i % 100 == 0)
+    self.postMessage({type: 'progress', current: i, max: 1000000});
+
+  chain = chainLength(i);
+
+  if (chain > longestChain) {
+    longestChain = chain;
+    longestChainStart = i;
+  }
+}
+
+self.postMessage({type: 'result',
+                  result: 'Longest chain start: ' + longestChainStart});
