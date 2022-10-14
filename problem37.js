b319696925daadbb882d8ecbd80ccd01f75c5aab
Boudah Talenka <boudah.talenka@gmail.com>
2022-10-14 16:14:34 +0200

    typo

--- problem37.js
+++ problem37.js
@@ -1,6 +1,6 @@
 /**
  * Truncatable primes (problem #37)
- * ========================================
+ * ================================
  * The number 3797 has an interesting property. Being prime itself, it is
  * possible to continuously remove digits from left to right, and remain prime
  * at each stage: 3797, 797, 97, and 7. Similarly we can work from right to
