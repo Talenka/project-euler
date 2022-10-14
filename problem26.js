1be9ca82c02f896a886e6eaabf001f8b03220f2b
Boudah Talenka <boudah.talenka@gmail.com>
2022-10-14 10:43:36 +0200

    Optimized solution

--- problem26.js
+++ problem26.js
@@ -1,5 +1,7 @@
 /**
- * Reciprocal cycles
+ * Reciprocal cycles (problem #26)
+ * ===============================
+ * 
  * A unit fraction contains 1 in the numerator. The decimal representation of
  * the unit fractions with denominators 2 to 10 are given:
  *
@@ -18,9 +20,56 @@
  *
  * Find the value of d < 1000 for which 1/d contains the longest recurring cycle
  * in its decimal fraction part.
-*/
+ *
+ * ========
+ * Solution
+ * ========
+ * If there is a reciprocal cycle for n > 1, it mean there we can write:
+ *
+ * 1/n = x * Sum(i=1 to Inf)[10**-(l*e)]
+ *
+ * With cycle x is of length l, i.e. l = Floor(log10(x)), and 1 <= x < 10**l
+ *
+ * Sum(i=1 to Inf)[1/(base**e)] = 1 / (base - 1), it is a well known result for
+ * geometric series, here with base = 10**length.
+ *
+ * We can write 1/n = x / (10 ** length - 1)
+ *
+ * Which mean (10 ** length - 1) is divisible by n. The reciprocal cycle for n
+ * is the lowest length for which (10 ** length - 1) is divisible by n.
+ *
+ * Optimization: we can note also that max length for a cycle is n - 1.
+ * Result: 983, with a reciprocal cycle of 982 digits.
+ */
 'use strict';
 
+
 /**
- * @todo
+ * @param {BigInt} max
  */
+function longestReciprocalCycleBelow(max) {
+  const cycles = {};
+
+  for (let n = 2n; n < max; n++) {
+    for (let length = n - 1n; length > 0; length--) {
+      if ((10n ** length - 1n) % n === 0n) {
+        cycles[n] = length;
+      }
+    }
+  }
+
+  let maxLength = 0n;
+  let maxCycle = 0n;
+
+  for (const n in cycles) {
+    if (cycles[n] > maxLength) {
+      maxLength = cycles[n];
+      maxCycle = n;
+    }
+  }
+
+  console.log(maxCycle, maxLength);
+  console.log(cycles);
+}
+
+longestReciprocalCycleBelow(1000n);
