fceda1a619f690ae10eb6ca3df9587de150f68e9
Boudah Talenka <boudah.talenka@gmail.com>
2022-10-14 16:15:05 +0200

    Add link into top jsdoc

--- problem26.js
+++ problem26.js
@@ -21,6 +21,8 @@
  * Find the value of d < 1000 for which 1/d contains the longest recurring cycle
  * in its decimal fraction part.
  *
+ * @see {@link https://projecteuler.net/problem=26}
+ *
  * ========
  * Solution
  * ========
