2785786cb1d2bed9d846af8ab9dcb7ea1450ffff
Boudah Talenka <boudah.talenka@gmail.com>
2022-10-14 17:59:58 +0200

    Solution

---
+++ problem39.js
@@ -0,0 +1,42 @@
+/**
+ * Integer right triangles (problem #39)
+ * =====================================
+ * If p is the perimeter of a right angle triangle with integral length sides,
+ * {a,b,c}, there are exactly three solutions for p = 120:
+ * {20,48,52}, {24,45,51}, {30,40,50}
+ *
+ * For which value of p ≤ 1000, is the number of solutions maximised?
+ *
+ * @see {@link https://projecteuler.net/problem=39}
+ *
+ * Solution: 840
+ */
+'use strict';
+
+
+/**
+ * @param {number} p
+ * @return {Object}
+ */
+function maxDiffPerimeters(p) {
+  const max = 1000;
+  const perimeters = [];
+  let pMax = 12; // 1st integer right triangle {3,4,5}
+
+  for (let a = 1; a <= max; a++) {
+    for (let b = a; b <= max; b++) {
+      for (let c = a; c <= max; c++) {
+        if (a*a + b*b === c*c) {
+          const p = a + b + c;
+          perimeters[p] = (p in perimeters) ? perimeters[p] + 1 : 1;
+          if (perimeters[p] > perimeters[pMax] && p < max) pMax = p;
+        }
+      }
+    }
+  }
+
+  console.log(perimeters[pMax]);
+  return pMax;
+}
+
+console.log(maxDiffPerimeters());
