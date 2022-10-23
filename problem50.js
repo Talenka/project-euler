16eeb0f41b200360c91893a0b0f49643983600e8
Boudah Talenka <boudah.talenka@gmail.com>
2022-10-23 21:08:55 +0200

    Solution 50

---
+++ problem50.js
@@ -0,0 +1,52 @@
+/**
+ * Consecutive prime sum (problem #50)
+ * ===================================
+ * The prime 41, can be written as the sum of six consecutive primes:
+ * 41 = 2 + 3 + 5 + 7 + 11 + 13
+ *
+ * This is the longest sum of consecutive primes that adds to a prime below
+ * one-hundred. The longest sum of consecutive primes below one-thousand that
+ * adds to a prime, contains 21 terms, and is equal to 953.
+ *
+ * Which prime, below one-million, can be written as the sum of the most
+ * consecutive primes?
+ *
+ * @see {@link https://projecteuler.net/problem=50}
+ * Solution: 997651
+ */
+'use strict';
+
+/**
+ * @param {BigInt} n
+ * @return {{maxSum: BigInt, maxLength: number}}
+ */
+function mostConsecutivePrimesSumUnder(n) {
+  const p = BigMath.primes;
+  let sum;
+  let length;
+  let maxSum = 0n;
+  let maxLength = 0;
+  let currentStart;
+
+  for (let i = 0, l = p.length; i < l; i++) {
+    [length, sum] = [0, 0n];
+    currentStart = i;
+
+    while (sum < n && i < l) {
+      sum += p[i];
+      if (p.indexOf(sum) !== -1) {
+        length = i - currentStart + 1;
+        if (length > maxLength) [maxLength, maxSum] = [length, sum];
+      }
+
+      i++;
+    }
+
+    i = currentStart;
+    if (sum > n) break;
+  }
+
+  return {maxSum: maxSum, maxLength: maxLength};
+}
+
+console.log(mostConsecutivePrimesSumUnder(1000000n));
