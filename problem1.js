84ab1172ee4a5da1ef882c1638d5ff6a32429c97
Boudah Talenka <boudah.talenka@gmail.com>
2022-10-24 17:36:40 +0200

    Simplification

--- problem1.js
+++ problem1.js
@@ -8,19 +8,9 @@
 
 /** @return {number} */
 function main() {
-  /** @type {number} Current integer (numbers < 3 are not 3 or 5 multiple) */
-  let n = 3;
-
-  /** @type {number} Sum of integers lower than current n */
   let sum = 0;
 
-  while (n < 1000) {
-    if (n % 3 === 0 || n % 5 === 0) {
-      sum += n;
-    }
-
-    n++;
-  }
+  for (let n = 3; n < 1000; n++) if (n % 3 === 0 || n % 5 === 0) sum += n;
 
   return sum;
 }
